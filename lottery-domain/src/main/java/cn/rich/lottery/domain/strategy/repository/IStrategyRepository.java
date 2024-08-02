f8652db9f70dcf1e376ac1fde9b5bf11abfd910d
Qi18 <181250087@smail.nju.edu.cn>
2024-08-02 10:53:09 +0800

    feat:策略领域初始化

---
+++ lottery-domain/src/main/java/cn/rich/lottery/domain/strategy/repository/IStrategyRepository.java
@@ -0,0 +1,9 @@
+package cn.rich.lottery.domain.strategy.repository;
+
+/**
+ * @author rich
+ * @version 1.0
+ * @create 2024/08/02
+ */
+public interface IStrategyRepository {
+}
---
+++ lottery-domain/src/main/java/cn/rich/lottery/domain/strategy/repository/Impl/StrategyRepository.java
@@ -0,0 +1,12 @@
+package cn.rich.lottery.domain.strategy.repository.Impl;
+
+import cn.rich.lottery.domain.strategy.repository.IStrategyRepository;
+
+/**
+ * @author rich
+ * @version 1.0
+ * @create 2024/08/02
+ */
+
+public class StrategyRepository implements IStrategyRepository {
+}
